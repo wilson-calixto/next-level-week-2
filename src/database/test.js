e4d03c552d0ce7587dcb5ccb0f5082f354d54528
Wilson Calixto Cordovil junior <17838558+wilson-calixto@users.noreply.github.com>
2020-08-10 20:46:58 -0400

    Add files via upload
    add all files

---
+++ src/database/test.js
@@ -0,0 +1,55 @@
+const dataBase = require('./db')
+const createProffy = require('./createProffy')
+
+dataBase.then(async (db) => {
+    // include data
+    proffyValue = {
+        name: 'Iago',
+        avatar: 'https://avatars3.githubusercontent.com/u/18369029?s=460&u=7e3fb7feaa50ada13dc7d004cf82817cea712fc3&v=4',
+        whatsapp: '123456',
+        bio: 'Instrutor de batata'
+    }
+
+    //  proffy_id included by dataBase
+    classValue = {
+        subject: 1,
+        cost: 40
+    }
+
+    // class_id included by dataBase
+    classScheduleValues = [{
+        weekday: 1,
+        time_from: 720,
+        time_to: 1220
+    }, {
+        weekday: 0,
+        time_from: 520,
+        time_to: 1220
+    }]
+
+    // await createProffy(db, { proffyValue, classValue, classScheduleValues })
+    // search data
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+    //console.log(selectedProffys)
+
+    const selectClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 1;
+    `)
+    //console.log(selectClassesAndProffys)
+
+    const selectedClassBySchedule = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = "1"
+        AND class_schedule.weekday = "0"
+        AND class_schedule.time_from <= "520"
+        AND class_schedule.time_to > "520"
+    `)
+
+    console.log(selectedClassBySchedule)
+
+
+})
